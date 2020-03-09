bbf14d03d137031a9717ce99449ff8a23f3a558a
Raul Coan <raul.coan@locaweb.com.br>
2020-03-09 08:34:44 -0300

    inital commit

---
+++ app/controllers/registry_callback_controller.rb
@@ -0,0 +1,2 @@
+class RegistryCallbackController < ApplicationController
+end
---
+++ test/controllers/registry_callback_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RegistryCallbackControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
