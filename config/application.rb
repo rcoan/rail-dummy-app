d3767b894fb9049e261ae5909ee2fe3cfb57c346
Raul Victor Coan <rvc.coan@gmail.com>
2023-09-21 10:14:07 -0300

    add host

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module VanillaApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.hosts << "721d-201-42-22-9.ngrok-free.app"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
